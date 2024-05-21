936270bb59044e95d1978cfcdeadc88e8ea5b6be
satra-11 <syuzuki546@gmail.com>
2024-05-21 12:05:01 +0900

    feat: added base layout

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <meta name="robots" content="noindex,follow"><!-- すべての検索エンジン用 -->
+        <meta name="googlebot" content="noindex,follow"><!-- Google用 -->
+
+        <!-- Styles -->
+        @livewireStyles
+    </head>
+    
+    @livewireScripts
+</html>
